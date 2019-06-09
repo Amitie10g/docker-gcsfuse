4cf1904c314d6bc1cf7ece94412062bbfdc4411f
Davod <davidkingnt@gmail.com>
2019-06-08 20:36:45 -0400

    Update startup-gcloud.sh

--- scripts/startup-gcloud.sh
+++ scripts/startup-gcloud.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 # Local Environment variables
-PUID=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['uid'])")
-PGID=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['gid'])")
-LOCAL_HOME=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['homeDirectory'])")
+HOME=/mnt/disks/qbittorrent
+CONF_PATH="$HOME/config"
+PUID="1000"
+PGID="1000"
 
-mkdir -p $LOCAL_HOME
-chown $PUID:$PGID $LOCAL_HOME
+# Create the directories
+mkdir -p "$CONF_PATH"
 
-# Download the Account service key from custom metadata
-curl -fSs "http://metadata.google.internal/computeMetadata/v1/instance/attributes/GCSKEY" \
--H "Metadata-Flavor: Google" -o "$LOCAL_HOME/gcs-key.json"
+# Download the Service account key from custom metadata
+curl -fSs "http://metadata.google.internal/computeMetadata/v1/instance/attributes/GCS_KEY" \
+-H "Metadata-Flavor: Google" -o "$CONF_PATH/gcs-key.json"
 
-# Check if the Service account key has been found
-if [ -f "$LOCAL_HOME/gcs-key.json" ]; then
-	chown "$PUID:$PGID" "$LOCAL_HOME/gcs-key.json"
-else
-	ERROR="Please set the custom metadata GCSKEY with your Service account key, or upload it to $LOCAL_HOME/gcs-key.json."
-	echo "$ERROR" >&2
-fi
+chown -R "$PUID:PGID" "$HOME"
+chmod -R 700 $CONF_PATH
